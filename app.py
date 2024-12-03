b790b17603227ca456e8ebf17c603696b0486332
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2024-12-03 23:52:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
+start_time = datetime.now()
+
 @app.route("/")
+def status():
+    return jsonify({"project": "kube-argocd", "start_time": start_time.isoformat()})
+
+@app.route("/hello")
 def hello_world():
     return "Hello, World!"
 

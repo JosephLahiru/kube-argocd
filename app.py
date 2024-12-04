b82a4f1f3f8c107d073ec4335ac29e91ac31013c
AlphaArchangel <joseph.rasanjana1@gmail.com>
2024-12-04 13:32:11 +0530

    Added waitress functioanlity

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, jsonify
 from datetime import datetime
+from waitress import serve
 
 app = Flask(__name__)
 
 start_time = datetime.now()
+
 
 @app.route("/")
 def status():
     print("processing status")
     return jsonify({"project": "kube-argocd", "start_time": start_time.isoformat()})
 
+
 @app.route("/hello")
 def hello_world():
     print("processing hello")
     return "Hello, World!"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print("API STARTED")
+    serve(app, host="0.0.0.0", port=5000)
